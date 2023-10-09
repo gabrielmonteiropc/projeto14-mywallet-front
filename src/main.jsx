bf4a61e5d77389d44a16a8de284d9d851eb734bb
gabrielmonteiropc <bhcmonteiro54@gmail.com>
2023-10-09 16:44:48 -0300

    teste para ver se passa no hub

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import ResetStyle from './style/ResetStyle';
+import GlobalStyle from './style/GlobalStyle';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
+    <ResetStyle />
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
